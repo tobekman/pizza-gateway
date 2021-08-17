3f7336c5d5de64f16c796a3a36447cb5f1e1b51f
Tub <nilstobiasekman@gmail.com>
2021-08-17 17:30:41 +0200

    Initial commit

---
+++ src/main/java/lab/webservices/pizzagateway/PizzaGatewayApplication.java
@@ -0,0 +1,13 @@
+package lab.webservices.pizzagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lab/webservices/pizzagateway/PizzaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.webservices.pizzagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
